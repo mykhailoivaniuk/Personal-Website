0998fdec51e4120c77952ef733dda068e13a62b2
Misha Ivaniuk <mykhailo.ivaniuk@gmail.com>
2021-12-17 12:49:47 +0200

    Animate theme change button

--- components/theme-button.js
+++ components/theme-button.js
@@ -1,14 +1,24 @@
 import { IconButton,useColorMode,useColorModeValue } from "@chakra-ui/react";
 import { SunIcon,MoonIcon } from "@chakra-ui/icons";
+import { AnimatePresence,motion } from "framer-motion";
 
 const ThemeButton = () => {
   const {toggleColorMode} = useColorMode();
   return (
-    <IconButton aria-label="Toggle Theme" 
-                colorScheme={useColorModeValue('purple','orange')} 
-                onClick={toggleColorMode}
-                icon={useColorModeValue(<MoonIcon/>, <SunIcon/>)}>
-    </IconButton>
+    <AnimatePresence exitBeforeEnter initial={false}>
+      <motion.div style={{display: 'inline-block'}}
+                  key={useColorModeValue('light','dark')}
+                  initial={{ x: -20, opacity: 0 }}
+                  animate={{ x: 0, opacity: 1 }}
+                  exit={{ x: 20, opacity: 0 }}
+                  transition={{duration: 0.2 }}>
+        <IconButton aria-label="Toggle Theme" 
+                    colorScheme={useColorModeValue('purple','orange')} 
+                    onClick={toggleColorMode}
+                    icon={useColorModeValue(<MoonIcon/>, <SunIcon/>)}>
+        </IconButton>
+      </motion.div>
+    </AnimatePresence>
   )
 
 }
