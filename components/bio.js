856923ad7efe88b18649eecb29f8928e7b0aff6c
Misha Ivaniuk <mykhailo.ivaniuk@gmail.com>
2021-12-17 12:38:12 +0200

    Add description on the main page

---
+++ components/bio.js
@@ -0,0 +1,22 @@
+import {Box} from '@chakra-ui/react';
+import styled from '@emotion/styled';
+
+export const BioSection = styled(Box)`
+  padding-left: 3.4em;
+  text-indent: -3.4em;
+`
+
+export const BioYear = styled.span `
+  font-weight: bold;
+  margin-right: 4.2em;
+`
+
+export const BioSeason = styled.span `
+  font-weight: bold;
+  margin-right: 2.4em;
+`
+
+export const Bio2Years = styled.span `
+  font-weight: bold;
+  margin-right: 1em;
+`
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,9 @@
-import {Container,Box,Heading,Image,useColorModeValue} from '@chakra-ui/react';
+import NextLink from 'next/link';
+import {Container,Button,Box,Heading,Image,useColorModeValue,Link} from '@chakra-ui/react';
 import Section from '../components/section';
-
+import Paragraph from '../components/paragraph';
+import { ExternalLinkIcon,ChevronRightIcon } from '@chakra-ui/icons';
+import { BioSection,BioYear,Bio2Years,BioSeason } from '../components/bio';
 const Page = () => {
   return (
     <Container>
@@ -12,9 +15,6 @@ const Page = () => {
           <Heading as='h2' variant="page-title">
             Misha Ivaniuk
           </Heading>
-          <p> Junior majoring in Computer Science and Statistics with experience in Python,
-            Javascript and Java interested in a way technology can be used to drive impact 
-            in all industries. </p>
         </Box>
         <Box flexShrink={1}  
             mt={{base: 4, md: 0}} 
@@ -32,7 +32,44 @@ const Page = () => {
       </Box>
     <Section delay={0.1}>
       <Heading as='h3' variant='section-title'> Work </Heading>
-      <p> Some work description </p>
+      <Paragraph> Junior majoring in Computer Science and Statistics with experience in Python,
+            Javascript and Java interested in a way technology can be used to drive impact 
+            in all industries. It all wasn&apos;t possible without{' '}
+            <Link href='https://www.ugs.foundation/' isExternal>
+            Ukraine Global Scholars <ExternalLinkIcon mx='2px' />
+            </Link>  
+      </Paragraph>
+      <Box align='center' my={4}>
+        <NextLink href='/projects'>
+          <Button rightIcon={<ChevronRightIcon/>} colorScheme='teal'>
+            My Projects
+          </Button>
+        </NextLink>
+      </Box>
+    </Section>
+    <Section delay={0.2}>
+      <Heading as='h3' variant='section-title'> Short Bio </Heading>
+      <BioSection>
+        <Bio2Years>2019-2023</Bio2Years> Combined BS/MS in Computer Science at Northwestern
+      </BioSection>
+      <BioSection>
+        <Bio2Years>2020-2021</Bio2Years> Intern at Kargo Technologies
+      </BioSection>
+      <BioSection>
+        <BioSeason>Fall 2021</BioSeason>Peer mentor for {' '}
+        <Link href='https://catalogs.northwestern.edu/undergraduate/courses-az/stat/' isExternal>Stat303 - Data Science</Link>
+      </BioSection>
+      <BioSection>
+        <BioSeason>Wint 2022</BioSeason>Peer mentor for {' '}
+        <Link href='https://www.mccormick.northwestern.edu/computer-science/academics/courses/descriptions/397-497-23.html' isExternal>
+        CS497 - Data Privacy</Link>
+      </BioSection>
+      <BioSection>
+        <BioYear>2022</BioYear>Intern at Palantir Technologies
+      </BioSection>
+    </Section>
+    <Section>
+    <Heading as='h3' variant='section-title'> Technologies I&apos;ve worked with </Heading>
     </Section>
     </Container>
     
