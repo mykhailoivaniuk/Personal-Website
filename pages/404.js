61eddb02f8577d5b85dd0dc812cfac1faa9d37d8
Misha Ivaniuk <mykhailo.ivaniuk@gmail.com>
2021-12-22 17:47:00 +0200

    Add custom 404 page

---
+++ pages/404.js
@@ -0,0 +1,25 @@
+import NextLink from 'next/link';
+import { Container,Box,Divider,Button,Text,Heading } from '@chakra-ui/react';
+
+const NotFound = () => {
+  return (
+    <Container>
+      <Box align='center' my={3}>
+        <Heading as='h1'>Page Not Found</Heading>
+        <Text> The page you&apos;re looking for does not exist yet. </Text>
+      </Box>
+      <Divider my={6}/>
+
+      <Box my={6} align='center'>
+        <NextLink href='/'>
+          <Button colorScheme='teal'>
+            Return to the home page
+          </Button>
+        </NextLink>
+      </Box>
+    </Container>
+  )
+
+}
+
+export default NotFound;
